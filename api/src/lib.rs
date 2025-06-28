bb81024166db2bc4519eddccaf2dcbb07ef9503d
danda <dan-da@users.noreply.github.com>
2025-06-28 08:04:15 +0000

    get rid of unwrap() in api crate

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -14,19 +14,19 @@ pub async fn echo(input: String) -> Result<String, ServerFnError> {
 
 #[server]
 pub async fn wallet_balance() -> Result<NativeCurrencyAmount, ServerFnError> {
-    let client = neptune_rpc::rpc_client().await;
-    let token = neptune_rpc::get_token().await;
+    let client = neptune_rpc::rpc_client().await?;
+    let token = neptune_rpc::get_token().await?;
 
-    let balance = client.confirmed_available_balance(tarpc::context::current(), token).await.unwrap().unwrap();
+    let balance = client.confirmed_available_balance(tarpc::context::current(), *token).await??;
     Ok(balance)
 }
 
 #[server(BlockHeightApi)]
 pub async fn block_height() -> Result<BlockHeight, ServerFnError> {
-    let client = neptune_rpc::rpc_client().await;
-    let token = neptune_rpc::get_token().await;
+    let client = neptune_rpc::rpc_client().await?;
+    let token = neptune_rpc::get_token().await?;
 
-    let height = client.block_height(tarpc::context::current(), token).await.unwrap().unwrap();
+    let height = client.block_height(tarpc::context::current(), *token).await??;
     Ok(height.into())
 }
 
@@ -34,6 +34,7 @@ pub async fn block_height() -> Result<BlockHeight, ServerFnError> {
 #[cfg(not(target_arch = "wasm32"))]
 mod neptune_rpc {
     use super::rpc_api;
+    use dioxus::prelude::ServerFnError;
 
     use std::net::Ipv4Addr;
     use std::net::SocketAddr;
@@ -48,28 +49,35 @@ mod neptune_rpc {
     use tarpc::tokio_serde::formats::Json;
     use tokio::sync::OnceCell;
 
-    async fn gen_rpc_client() -> rpc_api::RPCClient {
+    async fn gen_rpc_client() -> Result<rpc_api::RPCClient, ServerFnError> {
         let server_socket = SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::LOCALHOST), 9799);
-        let transport = tarpc::serde_transport::tcp::connect(server_socket, Json::default).await.unwrap();
+        let transport = tarpc::serde_transport::tcp::connect(server_socket, Json::default).await?;
 
-        rpc_api::RPCClient::new(client::Config::default(), transport).spawn()
+        Ok(rpc_api::RPCClient::new(client::Config::default(), transport).spawn())
     }
 
-    pub async fn rpc_client() -> &'static rpc_api::RPCClient {
-        static STATE: OnceCell<rpc_api::RPCClient> = OnceCell::const_new();
+    pub async fn rpc_client() -> Result<&'static rpc_api::RPCClient, ServerFnError> {
+        static STATE: OnceCell<Result<rpc_api::RPCClient, ServerFnError>> = OnceCell::const_new();
 
-        STATE.get_or_init(|| async { gen_rpc_client().await } ).await
+        STATE.get_or_init(|| async { gen_rpc_client().await } ).await.as_ref().map_err(|err| err.clone())
     }
 
-    pub async fn cookie_hint() -> rpc_auth::CookieHint {
-        let client = rpc_client().await;
-        client.cookie_hint(context::current()).await.unwrap().unwrap()
+    pub async fn cookie_hint() -> Result<rpc_auth::CookieHint, ServerFnError> {
+        let client = rpc_client().await?;
+        Ok(client.cookie_hint(context::current()).await??)
     }
 
-    pub(super) async fn get_token() -> rpc_auth::Token {
-        let hint = cookie_hint().await;
-        rpc_auth::Cookie::try_load(&hint.data_directory).await.unwrap().into()
+    async fn gen_token() -> Result<rpc_auth::Token, ServerFnError> {
+        let hint = cookie_hint().await?;
+        Ok(rpc_auth::Cookie::try_load(&hint.data_directory).await?.into())
     }
+
+    pub async fn get_token() -> Result<&'static rpc_auth::Token, ServerFnError> {
+        static STATE: OnceCell<Result<rpc_auth::Token, ServerFnError>> = OnceCell::const_new();
+
+        STATE.get_or_init(|| async { gen_token().await } ).await.as_ref().map_err(|err| err.clone())
+    }
+
 }
 
 // let rpc_auth::CookieHint {
