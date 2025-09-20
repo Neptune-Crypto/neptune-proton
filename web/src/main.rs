8753615c22c9d14eada590f70249695029eae83e
danda <dan-da@users.noreply.github.com>
2025-09-20 01:43:23 +0000

    chore: remove unused dep

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,10 +1,6 @@
 use dioxus::prelude::*;
 
 fn main() {
-
-    #[cfg(target_arch = "wasm32")]
-    console_error_panic_hook::set_once();    
-
     dioxus_logger::init(dioxus_logger::tracing::Level::INFO).expect("failed to init logger");
     dioxus::launch(App);
 }
